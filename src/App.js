351ce69e9ae874c559bed1342e6c6ceb72892d9a
Harshil Doshi <harshildoshi153@gmail.com>
2024-09-11 15:52:21 +0530

    a

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ export default class App extends Component {
         <div>
           <Navbar /> {/* Navbar component with links */}
           <Routes>
-            <Route path="/" element={<News pageSize = {15} category="general" title="General"/>} /> {/* Home page */}
-            <Route path="/sports" element={<News pageSize = {15} category="sports" title="Sports"/>} /> {/* sports news */}
-            <Route path="/business" element={<News pageSize = {15} category="business" title="Business"/>} /> {/* business news */}
-            <Route path="/entertainment" element={<News pageSize = {15} category="entertainment" title="Entertainment"/>} /> {/* entertainment news */}
-            <Route path="/science" element={<News pageSize = {15} category="science" title="Science"/>} /> {/* science news */}
-            <Route path="/technology" element={<News pageSize = {15} category="technology" title="Technology"/>} /> {/* technology */}
-            <Route path="/health" element={<News pageSize = {15} category="health" title="Health"/>} /> {/* health news */}
-            <Route path="/about" element={<About />} /> {/* About component */}
+            <Route path="/newsapp" element={<News pageSize = {15} category="general" title="General"/>} /> {/* Home page */}
+            <Route path="/newsapp/sports" element={<News pageSize = {15} category="sports" title="Sports"/>} /> {/* sports news */}
+            <Route path="/newsapp/business" element={<News pageSize = {15} category="business" title="Business"/>} /> {/* business news */}
+            <Route path="/newsapp/entertainment" element={<News pageSize = {15} category="entertainment" title="Entertainment"/>} /> {/* entertainment news */}
+            <Route path="/newsapp/science" element={<News pageSize = {15} category="science" title="Science"/>} /> {/* science news */}
+            <Route path="/newsapp/technology" element={<News pageSize = {15} category="technology" title="Technology"/>} /> {/* technology */}
+            <Route path="/newsapp/health" element={<News pageSize = {15} category="health" title="Health"/>} /> {/* health news */}
+            <Route path="/newsapp/about" element={<About />} /> {/* About component */}
           </Routes>
         </div>
       </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,31 +6,31 @@ export default class Navbar extends Component {
             <div className="sticky-top">
                 <nav className="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
                     <div className="container-fluid">
-                        <a className="navbar-brand text-light" href="/">News App</a>
+                        <a className="navbar-brand text-light" href="/newsapp">News App</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active text-light" aria-current="page" href="/" to="/">Home</a>
+                                    <a className="nav-link active text-light" aria-current="page" href="/newsapp" to="/newsapp">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link text-light" href="/about" to="/about">About</a>
+                                    <a className="nav-link text-light" href="/newsapp/about" to="/newsapp/about">About</a>
                                 </li>
                                 <li className="nav-item dropdown ">
-                                    <a className="nav-link dropdown-toggle text-light" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a className="nav-link dropdown-toggle text-light" href="/newsapp" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         Categories
                                     </a>
                                     <ul className="dropdown-menu">
-                                        <li><a className="dropdown-item" href="/sports">Sports</a></li>
-                                        <li><a className="dropdown-item" href="/business">Business</a></li>
+                                        <li><a className="dropdown-item" href="/newsapp/sports">Sports</a></li>
+                                        <li><a className="dropdown-item" href="/newsapp/business">Business</a></li>
                                         <li><hr className="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="/entertainment">Entertainment</a></li>
-                                        <li><a className="dropdown-item" href="/" to="/">General</a></li>
-                                        <li><a className="dropdown-item" href="/science">Science</a></li>
-                                        <li><a className="dropdown-item" href="/technology">Technology</a></li>
-                                        <li><a className="dropdown-item" href="/health">health</a></li>
+                                        <li><a className="dropdown-item" href="/newsapp/entertainment">Entertainment</a></li>
+                                        <li><a className="dropdown-item" href="/newsapp" to="/newsapp">General</a></li>
+                                        <li><a className="dropdown-item" href="/newsapp/science">Science</a></li>
+                                        <li><a className="dropdown-item" href="/newsapp/technology">Technology</a></li>
+                                        <li><a className="dropdown-item" href="/newsapp/health">health</a></li>
                                     </ul>
                                 </li>
                             </ul>
