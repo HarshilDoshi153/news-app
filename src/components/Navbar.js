9918974c3ed6a11ba6d9b8975ce0bc735f0add86
Harshil Doshi <harshildoshi153@gmail.com>
2024-09-11 13:22:47 +0530

    h

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default class Navbar extends Component {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active text-light" aria-current="page" href="/" to="/">Home</a>
+                                    <a className="nav-link active text-light" aria-current="page" href="/" to="/">House</a>
                                 </li>
                                 <li className="nav-item">
                                     <a className="nav-link text-light" href="/about" to="/about">About</a>
